7a7667805aa1ade6c3d40518b35cca15e5cb65e9
igush <igush@DESKTOP-3OTTB5S>
2022-08-29 01:29:57 +0300

    init

---
+++ src/lib/compress.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <istream>
+#include <ostream>
+
+void Archive() {
+
+}
---
+++ src/main/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <random>
+#include <algorithm>
+#include <chrono>
+#include "compress.h"
+
+
+int main() {
+
+  return 0;
+}
